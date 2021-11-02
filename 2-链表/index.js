0d884723c8f6f369fb6eba06319f65d9426e2a5c
bingxu <984386667@qq.com>
2021-11-02 11:36:11 +0800

    [update] 1. 删除链表倒数第n个节点；2. 判断链表是否相交；3. 判断链表是否有环，并返回环的入口

--- 2-链表/index.js
+++ 2-链表/index.js
@@ -226,3 +226,115 @@ var swapPairs = function(head) {
     }
     return res.next;
 };
+
+
+/**
+ * 19. 删除链表倒数第n个节点 https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/submissions/
+ * 快慢指针 + 虚头节点
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function(head, n) {
+    // 使用虚头节点可以避免对链表的头结点操作时出现问题
+    let newList = new ListNode(0, head);
+    let [slow, fast] = [newList, newList];
+    while (fast && fast.next) {
+        fast = fast.next;
+        if (n > 0) {
+            // 让fast节点比slow节点多走n个节点，那么fast为要删除的节点的pre节点
+            n -= 1;
+        } else {
+            slow = slow.next;
+        }
+    }
+    if (n === 0 && slow) {
+        slow.next = slow.next.next;
+    }
+    return newList.next;
+};
+// 暴力法，空间复杂度较大
+var removeNthFromEnd = function(head, n) {
+    const nodeArr = [];
+    // 用一个数组来保存链表节点，这样就可以在遍历一遍的情况下知道
+    // 1. 链表长度
+    // 2. 节点所在索引
+    let newHead = new ListNode(0, head);
+    let cur = newHead;
+    while (cur) {
+        nodeArr.push(cur);
+        cur = cur.next;
+    }
+
+    let pre = nodeArr[nodeArr.length - n - 1];
+    if (pre) {
+        pre.next = pre.next.next;
+    }
+    return newHead.next;
+};
+
+
+/**
+ * 链表相交 https://leetcode-cn.com/problems/intersection-of-two-linked-lists-lcci/submissions/
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+ var getIntersectionNode = function(headA, headB) {
+    let [curA, curB] = [headA, headB];
+    // 最多遍历2遍即可得到结果
+    while (curA !== curB) {
+        curA = curA ? curA.next : headB;
+        curB = curB ? curB.next : headA;
+    }
+    return curA;
+};
+
+
+/**
+ * 142. 环形链表2 https://leetcode-cn.com/problems/linked-list-cycle-ii/submissions/
+ * 暴力法，时间复杂度O(n)，空间复杂度O(n)
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    const set = new Set();
+    let cur = head;
+    while (cur) {
+        // 使用set的唯一性来检测节点是否重复
+        if (set.has(cur)) {
+            return cur;
+        } else {
+            set.add(cur);
+            cur = cur.next;
+        }
+    }
+    return null;
+};
+// 快慢指针，时间复杂度O(n)，空间复杂度O(1)
+var detectCycle = function(head) {
+    let [slow, fast] = [head, head];
+    let target, res = null;
+    while (fast) {
+        // fast节点为slow节点速度的2倍
+        fast = fast?.next?.next;
+        slow = slow?.next;
+        if (slow == fast) {
+            // 找到相遇节点，说明有环
+            target = fast;
+            break;
+        }
+    }
+    
+    if (target) {
+        // slow从头结点触发，fast从相遇节点出发，相遇的节点即为环的入口节点
+        slow = head;
+        fast = target;
+        while (slow !== fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        res = fast;
+    }
+    return res;
+};
